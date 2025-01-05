dd3ea29590d315de85b65913c0b9cec2c96ba29f
kryparnold <emirhanpgt@gmail.com>
2025-01-05 16:46:14 +0300

    change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Nextjs Starter",
+    title: "Biliyor Muydun?",
     description: "Built by emirhanpisgin",
 };
 

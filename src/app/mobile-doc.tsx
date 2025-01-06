f3f410d7e5e677819643054102fd433e8ae59c0b
kryparnold <emirhanpgt@gmail.com>
2025-01-06 19:36:03 +0300

    refactor: update MobileDoc component layout and replace Bell icon with ModeToggle

--- src/app/mobile-doc.tsx
+++ src/app/mobile-doc.tsx
@@ -1,20 +1,21 @@
-import { Bell, House } from 'lucide-react';
+import { House } from "lucide-react";
 import Link from "next/link";
 import CheckUserButton from "../components/check-user-button";
 import { auth } from "@/lib/auth";
+import ModeToggle from "@/components/mode-toggle-mobile";
 
 
 export default async function MobileDoc() {
     const session = await auth();
 
     return (
-        <div className="w-full h-20 fixed p-3 bottom-0 left-0 lg:hidden">
-            <div className="w-full h-full rounded-full border flex gap-[10%] justify-center items-center">
-                <CheckUserButton authorized={!!session?.user}/>
+        <div className="w-full fixed bottom-0 left-0 lg:hidden">
+            <div className="w-full h-full py-4 border-t flex gap-[15%] justify-center items-center">
+                <CheckUserButton authorized={!!session?.user} />
                 <Link href="/">
-                    <House className="size-8 text-foreground drop-shadow active:drop-shadow-none active:scale-90 transition-all" />
+                    <House className="size-8 text-foreground transition-all" />
                 </Link>
-                <Bell className="size-8 text-foreground drop-shadow active:drop-shadow-none active:scale-90 transition-all" />
+                <ModeToggle />
             </div>
         </div>
     );

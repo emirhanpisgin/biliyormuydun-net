ec8f9a0f9b2f2b92fc8a174b09ab470a0948f7fa
kryparnold <emirhanpgt@gmail.com>
2025-01-08 17:03:45 +0300

    feat: add ProfilePhoto component for user profile images

---
+++ src/components/profile-photo.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { cn } from "@/lib/utils";
+import React from 'react';
+
+interface ProfilePhotoProps extends React.HTMLProps<HTMLDivElement> {
+    url: string;
+}
+
+export default function ProfilePhoto({ url, className }: ProfilePhotoProps) {
+
+    return (
+        <div className={cn("size-24 relative", className)}>
+            <div className="size-24 bg-primary/10 animate-pulse rounded-full absolute top-0 left-0" />
+            <img
+                src={url}
+                alt="user profile photo"
+                className={"rounded-full absolute size-24"}
+            />
+        </div>
+    );
+}

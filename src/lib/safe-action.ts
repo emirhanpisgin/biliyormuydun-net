e7b212ba7398f84d9c3547591f87fd63356ddd42
kryparnold <emirhanpgt@gmail.com>
2025-01-10 00:55:03 +0300

    feat: cast session user to User type in authenticatedAction

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,10 +1,11 @@
 import { createServerActionProcedure } from "zsa";
 import { auth } from "./auth";
+import { User } from "@/db/schema";
 
 export const authenticatedAction = createServerActionProcedure().handler(async () => {
 	const session = await auth();
 	if (!session?.user) throw "Unauthenticated";
-	return session.user;
+	return session.user as User;
 });
 
 export const unauthenticatedAction = createServerActionProcedure();

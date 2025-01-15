e16f90b07373127973dd20c26f79ed30fcbbd174
kryparnold <emirhanpgt@gmail.com>
2025-01-15 21:16:38 +0300

    fix: update MobileDoc component styles for improved layout and responsiveness

--- src/app/_components/mobile-doc.tsx
+++ src/app/_components/mobile-doc.tsx
@@ -8,8 +8,8 @@ export default async function MobileDoc() {
     const session = await auth();
 
     return (
-        <div className="w-full fixed bottom-0 left-0 lg:hidden">
-            <div className="w-full h-full py-4 border-t flex gap-[15%] justify-center items-center">
+        <div className="w-full fixed bottom-0 left-0 lg:hidden p-2">
+            <div className="w-full h-full py-4 border flex gap-[15%] rounded-lg justify-center items-center">
                 <ProfileButton initialUser={session?.user} />
                 <Link href="/">
                     <House className="size-8 text-foreground transition-all" />

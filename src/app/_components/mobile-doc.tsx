6db98df03b5ca1b261d79e80663b7357a0962379
kryparnold <emirhanpgt@gmail.com>
2025-01-15 22:13:50 +0300

    fix: reorder ProfileButton and Home Link components for improved layout in MobileDoc

--- src/app/_components/mobile-doc.tsx
+++ src/app/_components/mobile-doc.tsx
@@ -10,14 +10,14 @@ export default async function MobileDoc() {
     return (
         <div className="w-full fixed bottom-0 left-0 lg:hidden p-2">
             <div className="w-full h-full py-4 border flex gap-[15%] rounded-lg justify-center items-center">
-                <ProfileButton initialUser={session?.user} />
                 <Link href="/">
                     <House className="size-8 text-foreground transition-all" />
                 </Link>
+                <ProfileButton initialUser={session?.user} />
                 <ModeToggle />
                 {session?.user?.role === "admin" && (
                     <Link href="/dashboard">
-                        <AppWindow className="size-8"/>
+                        <AppWindow className="size-8" />
                     </Link>
                 )}
             </div>

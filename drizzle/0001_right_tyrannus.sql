bff0e47fedfaed5dc3afc2c6a3e22d59b39f4ec9
kryparnold <emirhanpgt@gmail.com>
2025-01-23 02:35:21 +0300

    refactor: remove obsolete SQL migration files and update .gitignore for drizzle

--- drizzle/0001_right_tyrannus.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "topic" DROP CONSTRAINT "topic_categoryId_category_id_fk";
---> statement-breakpoint
-ALTER TABLE "topic" ADD CONSTRAINT "topic_categoryId_category_id_fk" FOREIGN KEY ("categoryId") REFERENCES "public"."category"("id") ON DELETE set null ON UPDATE no action;
--- drizzle/0002_dapper_mach_iv.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "topic" DROP CONSTRAINT "topic_authorId_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "topic" ALTER COLUMN "categoryId" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "topic" ALTER COLUMN "authorId" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "topic" ADD COLUMN "writerId" text;--> statement-breakpoint
-ALTER TABLE "topic" ADD CONSTRAINT "topic_writerId_user_id_fk" FOREIGN KEY ("writerId") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "topic" ADD CONSTRAINT "topic_authorId_user_id_fk" FOREIGN KEY ("authorId") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;
